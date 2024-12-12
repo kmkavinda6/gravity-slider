1fef797815d307b3eed4a9c5e0a2da606115958f
kmkavinda6 <kmkavinda6@gmail.com>
2024-12-12 23:52:29 +0530

    fixed particles and touch slider

--- client/src/components/Particles.js
+++ client/src/components/Particles.js
@@ -1,191 +1,73 @@
 import { useCallback, useEffect, useState } from "react";
-
 import Particles, { initParticlesEngine } from "@tsparticles/react";
-
-
-
 import { loadFull } from "tsparticles";
+import React from "react";
 
-export default function Particle() {
-
+const Particle = () => {
     const [init, setInit] = useState(false);
 
     useEffect(() => {
-
         console.log("init");
-
         initParticlesEngine(async (engine) => {
-
             await loadFull(engine);
-
         }).then(() => {
-
             setInit(true);
-
         });
-
     }, []);
 
-
-
-    const particlesLoaded = (container) => {
-
-    };
-
-
+    const particlesLoaded = (container) => {};
 
     return (
-
         <>
-
             {init && (
-
                 <Particles
-
                     id="tsparticles"
-
                     particlesLoaded={particlesLoaded}
-
-                    style={{
-
-                        zIndex: 1,
-
-                    }}
-
+                    style={{ zIndex: 1 }}
                     options={{
-
                         fpsLimit: 120,
-
                         interactivity: {
-
                             events: {
-
-                                onClick: {
-
-                                    enable: true,
-
-                                    mode: "push",
-
-                                },
-
-                                onHover: {
-
-                                    enable: true,
-
-                                    mode: "repulse",
-
-                                },
-
+                                onClick: { enable: true, mode: "push" },
+                                onHover: { enable: true, mode: "repulse" },
                                 resize: true,
-
                             },
-
                             modes: {
-
-                                push: {
-
-                                    quantity: 4,
-
-                                },
-
-                                repulse: {
-
-                                    distance: 200,
-
-                                    duration: 0.4,
-
-                                },
-
+                                push: { quantity: 4 },
+                                repulse: { distance: 200, duration: 0.4 },
                             },
-
                         },
-
                         particles: {
-
-                            color: {
-
-                                value: "#ffffff",
-
-                            },
-
+                            color: { value: "#ffffff" },
                             links: {
-
                                 color: "#ffffff",
-
                                 distance: 150,
-
                                 enable: true,
-
                                 opacity: 0.5,
-
                                 width: 1,
-
                             },
-
                             move: {
-
                                 direction: "none",
-
                                 enable: true,
-
-                                outModes: {
-
-                                    default: "bounce",
-
-                                },
-
+                                outModes: { default: "bounce" },
                                 random: false,
-
                                 speed: 1.2,
-
                                 straight: false,
-
                             },
-
                             number: {
-
-                                density: {
-
-                                    enable: true,
-
-                                    area: 800,
-
-                                },
-
+                                density: { enable: true, area: 800 },
                                 value: 160,
-
                             },
-
-                            opacity: {
-
-                                value: 0.5,
-
-                            },
-
-                            shape: {
-
-                                type: "circle",
-
-                            },
-
-                            size: {
-
-                                value: { min: 1, max: 5 },
-
-                            },
-
+                            opacity: { value: 0.5 },
+                            shape: { type: "circle" },
+                            size: { value: { min: 1, max: 5 } },
                         },
-
                         detectRetina: true,
-
                     }}
-
                 />
-
             )}
-
         </>
-
     );
+};
 
-}
+export default React.memo(Particle);
